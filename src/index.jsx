210320b4ee6384484ea0a9e14552ded75f25ed43
Hritik <63942266+hrshiti@users.noreply.github.com>
2024-11-29 09:31:47 +0000

    new portfolio website
    thi
    s is the update website of portfolio

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+
 import App from './App'
+// import {
+// 	createBrowserRouter,
+// 	RouterProvider,
+// } from "react-router-dom";
+
+
+
+
+	// {
+	// 	path: "/",
+	// 	element: <App />,
+	// },
+	// {
+	// 	path: "navbar",
+	// 	element: <Navbar />,
+	// },
+	// {
+	// 	path: "home",
+	// 	element: <Home />,
+	// },
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
+		{/* <RouterProvider router={router} /> */}
 		<App />
 	</React.StrictMode>
 )
